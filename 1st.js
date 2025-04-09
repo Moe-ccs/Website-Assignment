e4f197ba53cb3232d1f77c9fa99ed1734f644d5c
Moe-ccs <businesstrip315@gmail.com>
2025-04-10 01:36:33 +0300

    Update 1st.js

--- 1st.js
+++ 1st.js
@@ -2,3 +2,9 @@
 let menu = document.querySelector('#menu-btn');
 // Getting the navbar element inside of the header
 let navbar = document.querySelector('.header .flex .navbar');
+menu.onclick = () =>{
+   // The code below toggles the icon to switch between the icon and close icon
+   menu.classList.toggle('fa-times');
+   // This code below toggles the navbar visibility
+   navbar.classList.toggle('active');
+}
