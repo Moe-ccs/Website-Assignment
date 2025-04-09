9d930030b7a1b414991d172b39ab3c9223317e62
Moe-ccs <businesstrip315@gmail.com>
2025-04-10 01:35:52 +0300

    Create 1st.js

---
+++ 1st.js
@@ -0,0 +1,4 @@
+// Showing the menu button
+let menu = document.querySelector('#menu-btn');
+// Getting the navbar element inside of the header
+let navbar = document.querySelector('.header .flex .navbar');
